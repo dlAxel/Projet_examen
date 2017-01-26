ffc358d4abe64e5e80ab5ac11653320f7baedc2b
Axel <dl_aznar@mode83.net>
2017-01-26 15:17:34 +0100

    Sauvegarde

--- PHP/function_addAccount.php
+++ PHP/function_addAccount.php
@@ -1,37 +1,36 @@
 <?php
 
-	include('config.php');
+include('config.php');
 
-	$name = ucfirst(strtolower($_POST['nom']))." ". ucfirst(strtolower($_POST['prenom']));
+$name = ucfirst(strtolower($_POST['nom']))." ". ucfirst(strtolower($_POST['prenom']));
 
-	if (empty ($name) || empty ($_POST['email']) || empty ($_POST['mdp']) || empty ($_POST['confirmMdp'])) {
+if (empty ($name) || empty ($_POST['email']) || empty ($_POST['mdp']) || empty ($_POST['confirmMdp'])) {
 
-        echo 'Veuillez renseigner tous les champs';
-        die();
-    }
+	echo 'Veuillez renseigner tous les champs';
+	die();
+}
 
-    if ($_POST['mdp'] == $_POST['confirmMdp']) {
-    	$nom = filter_var($_POST['nom'],FILTER_SANITIZE_STRING);
-		$prenom = filter_var($_POST['prenom'],FILTER_SANITIZE_STRING);
-		$email = filter_var($_POST['email'],FILTER_SANITIZE_STRING);
-		$mdp = filter_var($_POST['mdp'],FILTER_SANITIZE_STRING);
-	    	createAccount($nom, $prenom, $email, $mdp);
-        echo 'Compte ajouté';
+if ($_POST['mdp'] == $_POST['confirmMdp']) {
+	$nom = filter_var($_POST['nom'],FILTER_SANITIZE_STRING);
+	$prenom = filter_var($_POST['prenom'],FILTER_SANITIZE_STRING);
+	$email = filter_var($_POST['email'],FILTER_SANITIZE_STRING);
+	$mdp = filter_var($_POST['mdp'],FILTER_SANITIZE_STRING);
+	createAccount($nom, $prenom, $email, $mdp);
+	echo 'Compte ajouté';
 
-    } else {
+} else {
 
-        echo 'Veuiller retaper votre mot de passe';
-    }	
+	echo 'Veuiller retaper votre mot de passe';
+}	
 
-	function createAccount($inLName, $inFName, $inMail, $inPassword) {
-		global $bdd;
-		$query = null; 
-		
-		$hash = password_hash ($inPassword, PASSWORD_BCRYPT);
-		$query = "INSERT INTO `compte`(`nom`, `prenom`, `email`, `mdp`) VALUES (".$bdd->quote($inLName).", ".$bdd->quote($inFName).", ".$bdd->quote($inMail).", ".$bdd->quote($hash).")";
-		
-		$bdd->exec($query);
-		header("Location: ../HTML/page_connexion.html");
-		exit(0);
-	}
+function createAccount($inLName, $inFName, $inMail, $inPassword) {
+	global $bdd;
+	$query = null; 
 	
+	$hash = password_hash ($inPassword, PASSWORD_BCRYPT);
+	$query = "INSERT INTO `compte`(`nom`, `prenom`, `email`, `mdp`) VALUES (".$bdd->quote($inLName).", ".$bdd->quote($inFName).", ".$bdd->quote($inMail).", ".$bdd->quote($hash).")";
+	
+	$bdd->exec($query);
+	header("Location: ../HTML/page_connexion.html");
+	exit(0);
+}
--- PHP/function_connexion.php
+++ PHP/function_connexion.php
@@ -1,40 +1,40 @@
 <?php
 
-	session_start();
-	var_dump($_REQUEST);
-	include('config.php');
+session_start();
+var_dump($_REQUEST);
+include('config.php');
 
-	$email = filter_var($_POST['email'],FILTER_SANITIZE_STRING);
-	$mdp = filter_var($_POST['mdp'],FILTER_SANITIZE_STRING);
+$email = filter_var($_POST['email'],FILTER_SANITIZE_STRING);
+$mdp = filter_var($_POST['mdp'],FILTER_SANITIZE_STRING);
 
-	//verify the textfield are empty if not empty he execute the function 
-	if (empty ($email) || empty($mdp)) {
-		echo "Remplie tout les champs pout te connecter.";
-	} else {
-		connexion($email, $mdp);
+//verify the textfield are empty if not empty he execute the function 
+if (empty ($email) || empty($mdp)) {
+	echo "Remplie tout les champs pout te connecter.";
+} else {
+	connexion($email, $mdp);
+}
+
+function connexion($inEmail, $inMdp) {
+	global $bdd;
+	$query = null;
+
+	$query = "SELECT `mdp` FROM `compte` WHERE `email` = ".$bdd->quote($inEmail);	
+	$sth = $bdd->query($query);
+	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+	if ($_POST === $result) {
+		$bdd->exec($query);
 	}
 
-	function connexion($inEmail, $inMdp) {
-		global $bdd;
-		$query = null;
-
-		$query = "SELECT `mdp` FROM `compte` WHERE `email` = ".$bdd->quote($inEmail);	
-		$sth = $bdd->query($query);
-		$result = $sth->fetchAll(PDO::FETCH_ASSOC);
-		
-		if ($_POST === $result) {
-			$bdd->exec($query);
-		}
-		
-		if (count($result) == 1) {
-			$test = password_verify($inMdp, $result[0]['mdp']);
-			if ($test) {
-				echo "Mot de passe correct";
-				header("Location: ../HTML/Page_daccueil.html");
-			} else {
-				echo "Mot de passe incorrect";
-			}	
-		}
+	if (count($result) == 1) {
+		$test = password_verify($inMdp, $result[0]['mdp']);
+		if ($test) {
+			echo "Mot de passe correct";
+			header("Location: ../HTML/page_principale.html");
+		} else {
+			echo "Mot de passe incorrect";
+		}	
 	}
+}
 
 //SELECT `email`, `mdp` FROM `compte`
