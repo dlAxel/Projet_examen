3cb19e653a43b21b8a4c507b3b3c587b85b4dba7
Axel <dl_aznar@mode83.net>
2017-03-14 16:02:13 +0100

    Sauvegarde

--- PHP/Page_creation_evenement.php
+++ PHP/Page_creation_evenement.php
@@ -7,7 +7,7 @@
 		<title>Page principale</title>
 	</head>
 	<body>
-        <form method="POST"></form>
+        <form action="function_creation_evenement.php" method="POST">
         
             <div class="sidebar1">
                 <div class="logo">
@@ -44,34 +44,97 @@
 
                     <div class="row">          
                         <div class="col-lg-offset-2 col-lg-5 center">
-                            <p>Nom de l'évènement : <input type="text" name="Nom">
-
-                            <p>Adresse de l'évènement : <input type="text" name="Adresse">
-
-                            <p>Ville : <input type="text" name="Ville">
-
-                            <p>Code postal : <input type="text" size="5" maxlength="5" name="CP">
+                            <table  width="100%">
+                                <tr>
+                                    <td class="text">
+                                        Nom de l'évènement :
+                                    </td>
+                                    <td class="champ">
+                                        <input type="text" name="Nom">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text">
+                                        Adresse de l'évènement :
+                                    </td>
+                                    <td class="champ"> 
+                                        <input type="text" name="Adresse">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text">
+                                        Ville : 
+                                    </td>
+                                    <td class="champ">
+                                        <input type="text" name="Ville">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text">
+                                        Code postal :
+                                    </td>
+                                    <td class="champ">
+                                        <input type="text" size="5" maxlength="5" name="CP">
+                                    </td>
+                                </tr>
+                            </table>    
                         </div> 
                     
                         <div class="col-lg-5 center">
-                            <p>Date de l'évènement : <input type="date" size="10" name="jour">
-
-                            <p>Heure de début : <input type="text" maxlength="2" size="2" name=""><span>h</span><input type="text" size="2" name="">
-
-                            <p>Heure de fin : <input type="text" maxlength="2" size="2" name=""><span>h</span><input type="text" size="2" name="">
-                        
-                            <form>
-                            
-                                <p class="menu">Mineur accepté :</p>
-                                
-                                    <select class="menu">
-                                        <option>oui
-                                        <option>non
-                                    </select>
-                            
-                            </form>
+                            <table>
+                                <tr>
+                                    <td class="text-2">
+                                        Date de l'évènement : 
+                                    </td>
+                                    <td class="champ-2">
+                                        <input type="date" size="10" name="jour">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="text-2">
+                                        Heure de début :
+                                    </td> 
+                                    <td class="champ-2">
+                                        <input type="text" maxlength="2" size="2" name=""><span>h</span><input type="text" size="2" name="">
+                                    </td>
+                                </tr>
+                                    <tr>
+                                    <td class="text-2">
+                                        Heure de fin :
+                                    </td>
+                                    <td class="champ-2">
+                                        <input type="text" maxlength="2" size="2" name=""><span>h</span><input type="text" size="2" name="">
+                                    </td>
+                                </tr>
+                                    <tr>
+                                        <form>
+                                            <td class="text-2">
+                                                Mineur accepté :
+                                            </td>
+                                            <td class="champ-2">
+                                                <select>
+                                                    <option>Oui
+                                                    <option>Non
+                                                </select>
+                                            </td>
+                                        </form>
+                                    </tr>
+                                    <tr>
+                                        <form>
+                                            <td class="text-2">
+                                                Visibilité :
+                                            </td>
+                                            <td class="champ-2">    
+                                               <select>
+                                                    <option>Public
+                                                    <option>Privé
+                                                </select>
+                                            </td>
+                                        </form>    
+                                    </tr>
+                                </div>
+                            </table>
                         </div>
-                    </div>
             <div class="row">
                 <div class="col-lg-offset-2 col-lg-10 center">
                     <p>Liste de chose a ramener : </p><textarea name="textarea"rows="4" cols="50"></textarea>
@@ -81,7 +144,8 @@
                     <p>Tenue requise : </p><textarea name="textarea"rows="4" cols="50"></textarea>                     
                 </div>
                 <div class="col-lg-offset-2 col-lg-10 center">
-                    <button type="submit" class="btn btn-success">Envoyer</button>
+                        <button type="submit" class="btn btn-success">Envoyer</button>
+                    </form>
                 </div>
             </div>                    	
         </div>
--- PHP/function_creation_evenement.php
+++ PHP/function_creation_evenement.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once('config.php');
+	require_once('config.php');
 
-if ($zevefv
+	envoie();
+
+	function envoie() {
+		INSERT INTO `evenement`(`nom_evenement`, `adresse_evenement`, `ville`, `code_postal`, `date_evenement`, `heure_debut`, `heure_fin`, `mineur_autorisee`, `public_prive`);
+		exec();
+	}
